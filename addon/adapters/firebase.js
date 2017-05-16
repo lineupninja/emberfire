98b337b7a3ef70eb77b2e7b1f4e708d528ebcc3a
Gordon Johnston <gordonj@foldedplane.com>
2017-05-16 17:59:31 +0100

    _internalModel needs to be retried with Ember.get as it might be DS.PromiseObject

--- addon/adapters/firebase.js
+++ addon/adapters/firebase.js
@@ -657,8 +657,8 @@ export default DS.Adapter.extend(Waitable, {
    * @return {Firebase}
    */
   _getAbsoluteRef(record) {
-    if (record._internalModel) {
-      record = record._internalModel;
+    if (Ember.get(record,'_internalModel')) {
+      record = Ember.get(record,'_internalModel');
     }
 
     var embeddingParent = this.getFirstEmbeddingParent(record);
